f83717fee4930f0c055874b0340088f1d40339a3
ehadzic <44297122+ehadzic@users.noreply.github.com>
2019-10-23 09:40:27 +0200

    Supermarket

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,45 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private final int maxBrojArtikala = 50;
+    private Artikl[] artikli = new Artikl[maxBrojArtikala];
+    private int brojArtikala = 0;
+
+    public int getMaxBrojArtikala() {
+        return maxBrojArtikala;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public void setArtikli(Artikl[] artikli) {
+        this.artikli = artikli;
+    }
+
+    public int getBrojArtikala() {
+        return brojArtikala;
+    }
+
+    public void setBrojArtikala(int brojArtikala) {
+        this.brojArtikala = brojArtikala;
+    }
+
+    public boolean dodajArtikl(Artikl artikl){
+
+        if(artikli.length < maxBrojArtikala){
+            artikli[brojArtikala] = artikl;
+            brojArtikala++;
+            return true;
+        }
+        return false;
+
+    }
+
+
+
+
+
+
+
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,57 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private final int maxBrojArtikala = 1000;
+    private Artikl[] artikli = new Artikl[maxBrojArtikala];
+    private int brojArtikala = 0;
+
+    public int getMaxBrojArtikala() {
+        return maxBrojArtikala;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public void setArtikli(Artikl[] artikli) {
+        this.artikli = artikli;
+    }
+
+    public int getBrojArtikala() {
+        return brojArtikala;
+    }
+
+    public void setBrojArtikala(int brojArtikala) {
+        this.brojArtikala = brojArtikala;
+    }
+
+    public boolean dodajArtikl(Artikl artikl) {
+
+        if (artikli.length < maxBrojArtikala) {
+            artikli[brojArtikala] = artikl;
+            brojArtikala++;
+            return true;
+        }
+        return false;
+    }
+
+    public Artikl izbaciArtikl(String kod){
+        Artikl artikl = new Artikl();
+        if(0 == artikli.length) System.out.println("Nema dostupnih artikala!");
+        for(int i = 0; i < brojArtikala; i++){
+            if (artikli[i].getKod().equals(kod)){
+                artikl = artikli[i];
+                for(int j = 0; j < brojArtikala - 1; j++){
+                    artikli[j] = artikli[j+1];
+                }
+                brojArtikala--;
+                artikli[brojArtikala] = null;
+                break;
+            }
+        }
+        return artikl;
+
+    }
+
+    
 }
